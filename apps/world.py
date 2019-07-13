3f40d306def85cedeab067ae49e6404d8a81723d
virav15 <47110597+virav15@users.noreply.github.com>
2019-07-13 15:49:03 -0400

    Update world.py

--- apps/world.py
+++ apps/world.py
@@ -1,6 +1,6 @@
 import random
 import textwrap
-import datetime as dt
+import datetime as dt 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -8,9 +8,9 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
 from app import app
-terrorism = pd.read_csv('apps/data/terrorism.csv',
-                        encoding='latin-1', low_memory=False,
-                        usecols=['iyear', 'imonth', 'iday', 'country_txt', 'city', 'longitude', 'latitude',
+terrorism = pd.read_csv('apps/data/terrorism.csv', 
+                        encoding='latin-1', low_memory=False, 
+                        usecols=['iyear', 'imonth', 'iday', 'country_txt', 'city', 'longitude', 'latitude', 
                         'nkill', 'nwound', 'summary', 'target1', 'gname'])
 
 terrorism = terrorism[terrorism['imonth'] != 0]
@@ -33,8 +33,8 @@ layout = html.Div([
                         dots=True,
                         value=[2010, 2016],
                         marks={str(yr): "'" + str(yr)[2:] for yr in range(1970, 2017)}),
-
-        html.Br(), html.Br(),
+         
+        html.Br(), html.Br(), 
     ], style={'width': '75%', 'margin-left': '12%', 'background-color': '#eeeeee'}),
     html.Div([
         dcc.Dropdown(id='countries',
@@ -42,12 +42,12 @@ layout = html.Div([
                      value=[''],
                      placeholder='Select Countries',
                      options=[{'label': c, 'value': c}
-                              for c in sorted(terrorism['country_txt'].unique())])
+                              for c in sorted(terrorism['country_txt'].unique())])        
     ], style={'width': '50%', 'margin-left': '25%', 'background-color': '#eeeeee'}),
-
+    
     dcc.Graph(id='by_year_country_world',
               config={'displayModeBar': False}),
-    html.Hr(),
+    html.Hr(), 
     html.Content('Top Countries', style={'margin-left': '45%', 'font-size': 25}),
     html.Br(), html.Br(),
     html.Div([
@@ -60,13 +60,13 @@ layout = html.Div([
                                 value=[2010, 2016],
                                 marks={str(yr): str(yr) for yr in range(1970, 2017, 5)}),
                 html.Br(),
-
+                
             ], style={'margin-left': '5%', 'margin-right': '5%'}),
             dcc.Graph(id='top_countries_attacks',
                       figure={'layout': {'margin': {'r': 10, 't': 50}}},
                       config={'displayModeBar': False})
         ], style={'width': '48%', 'display': 'inline-block'}),
-
+        
         html.Div([
             html.Div([
                 dcc.RangeSlider(id='years_deaths',
@@ -76,7 +76,7 @@ layout = html.Div([
                                 value=[2010, 2016],
                                 marks={str(yr): str(yr) for yr in range(1970, 2017, 5)}),
                 html.Br(),
-
+                
             ], style={'margin-left': '5%', 'margin-right': '5%'}),
 
             dcc.Graph(id='top_countries_deaths',
@@ -85,14 +85,14 @@ layout = html.Div([
 
         ], style={'width': '48%', 'display': 'inline-block', 'float': 'right'})
     ]),
-
     
+    html.A('@eliasdabbas', href='https://www.twitter.com/eliasdabbas'), 
     html.P(),
     html.Content('  Code: '),
-
+    html.A('github.com/eliasdabbas/terrorism', href='https://github.com/eliasdabbas/terrorism'), html.Br(), html.Br(),
     html.Content('Data: National Consortium for the Study of Terrorism and Responses to Terrorism (START). (2016). '
                  'Global Terrorism Database [Data file]. Retrieved from https://www.start.umd.edu/gtd')
-
+    
 ], style={'background-color': '#eeeeee', 'font-family': 'Palatino'})
 
 @app.callback(Output('by_year_country_world', 'figure'),
@@ -100,10 +100,10 @@ layout = html.Div([
 def annual_by_country_barchart(countries, years):
     df = terrorism[terrorism['country_txt'].isin(countries) & terrorism['iyear'].between(years[0], years[1])]
     df = df.groupby(['iyear', 'country_txt'], as_index=False)['date'].count()
-
+    
     return {
         'data': [go.Bar(x=df[df['country_txt'] == c]['iyear'],
-                        y=df[df['country_txt'] == c]['date'],
+                        y=df[df['country_txt'] == c]['date'], 
                         name=c)
                  for c in countries] ,
         'layout': go.Layout(title='Yearly Terrorist Attacks ' + ', '.join(countries) + '  ' + ' - '.join([str(y) for y in years]),
@@ -116,7 +116,7 @@ def annual_by_country_barchart(countries, years):
              [Input('countries', 'value'), Input('years', 'value')])
 def countries_on_map(countries, years):
     df = terrorism[terrorism['country_txt'].isin(countries) & terrorism['iyear'].between(years[0], years[1])]
-
+    
     return {
         'data': [go.Scattergeo(lon=[x + random.gauss(0.04, 0.03) for x in df[df['country_txt'] == c]['longitude']],
                                lat=[x + random.gauss(0.04, 0.03) for x in df[df['country_txt'] == c]['latitude']],
@@ -126,9 +126,9 @@ def countries_on_map(countries, years):
                                hovertext=df[df['country_txt'] == c]['city'].astype(str) + ', ' + df[df['country_txt'] == c]['country_txt'].astype(str)+ '<br>' +
                                          [dt.datetime.strftime(d, '%d %b, %Y') for d in df[df['country_txt'] == c]['date']] + '<br>' +
                                          'Perpetrator: ' + df[df['country_txt'] == c]['gname'].astype(str) + '<br>' +
-                                         'Target: ' + df[df['country_txt'] == c]['target1'].astype(str) + '<br>' +
+                                         'Target: ' + df[df['country_txt'] == c]['target1'].astype(str) + '<br>' + 
                                          'Deaths: ' + df[df['country_txt'] == c]['nkill'].astype(str) + '<br>' +
-                                         'Injured: ' + df[df['country_txt'] == c]['nwound'].astype(str) + '<br><br>' +
+                                         'Injured: ' + df[df['country_txt'] == c]['nwound'].astype(str) + '<br><br>' + 
                                          ['<br>'.join(textwrap.wrap(x, 40)) if not isinstance(x, float) else '' for x in df[df['country_txt'] == c]['summary']])
                  for c in countries],
         'layout': go.Layout(title='Terrorist Attacks ' + ', '.join(countries) + '  ' + ' - '.join([str(y) for y in years]),
@@ -138,9 +138,9 @@ def countries_on_map(countries, years):
                             plot_bgcolor='#eeeeee',
                             width=1420,
                             height=650,
-                            annotations=[{'text': 'gtd', 'x': .2, 'y': -.1,
+                            annotations=[{'text': '<a href="https://www.twitter.com">@eliasdabbas</a>', 'x': .2, 'y': -.1, 
                                           'showarrow': False},
-                                         {'text': 'Data: START Consortium', 'x': .2, 'y': -.13, 'showarrow': False}],
+                                         {'text': 'Data: START Consortium', 'x': .2, 'y': -.13, 'showarrow': False}],                            
                             geo={'showland': True, 'landcolor': '#eeeeee',
                                  'countrycolor': '#cccccc',
                                  'showsubunits': True,
@@ -149,7 +149,7 @@ def countries_on_map(countries, years):
                                  'showcountries': True,
                                  'oceancolor': '#eeeeee',
                                  'showocean': True,
-                                 'showcoastlines': True,
+                                 'showcoastlines': True, 
                                  'showframe': False,
                                  'coastlinecolor': '#cccccc',
                                  'lonaxis': {'range': [df['longitude'].min()-1, df['longitude'].max()+1]},
