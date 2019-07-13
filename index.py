5f00567ecbf81a2b906306cbadb3c0bcb4b0b9bf
virav15 <47110597+virav15@users.noreply.github.com>
2019-07-13 15:49:11 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
@@ -7,26 +6,17 @@ from app import app
 server = app.server
 from apps import world, country
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content'),
-    # dcc.ConfirmDialog(
-    #     id='confirm',
-    #     message='Danger danger! Are you sure you want to continue?'
-    # )
+    html.Div(id='page-content')
 ])
-
-
 
 @app.callback(Output('page-content', 'children'),
              [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/country':
         return country.layout
-
     else:
         return world.layout
 
