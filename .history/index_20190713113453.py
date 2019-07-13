3f6d3ee638e3ddd168ac2f878413dfdd7a0627fa
virav15 <47110597+virav15@users.noreply.github.com>
2019-07-13 13:36:12 -0400

    --- Auto-Git Commit ---

---
+++ .history/index_20190713113453.py
@@ -0,0 +1,28 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app
+server = app.server
+from apps import world, country
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+@app.callback(Output('page-content', 'children'),
+             [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/country':
+        return country.layout
+
+    else:
+        return world.layout
+
+if __name__ == '__main__':
+    app.run_server()
---
+++ .history/index_20190713113904.py
@@ -0,0 +1,33 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app
+server = app.server
+from apps import world, country
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+app.layout = html.Div([
+    dcc.ConfirmDialog(
+        id='confirm',
+         )
+
+@app.callback(Output('page-content', 'children'),
+             [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/country':
+        return country.layout
+
+    else:
+        return world.layout
+
+if __name__ == '__main__':
+    app.run_server()
