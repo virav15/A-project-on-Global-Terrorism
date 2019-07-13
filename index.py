9cc3040d5f3b9a2b87cd74c496af8fca6a0e121e
virav15 <47110597+virav15@users.noreply.github.com>
2019-07-13 13:19:41 -0400

    Update index.py

--- index.py
+++ index.py
@@ -7,11 +7,19 @@ from app import app
 server = app.server
 from apps import world, country
 
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
+    html.Div(id='page-content'),
+    # dcc.ConfirmDialog(
+    #     id='confirm',
+    #     message='Danger danger! Are you sure you want to continue?'
+    # )
 ])
+
+
 
 @app.callback(Output('page-content', 'children'),
              [Input('url', 'pathname')])
