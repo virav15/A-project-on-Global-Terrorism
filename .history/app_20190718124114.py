bc6554a2f9c5056fe0a464fb91cedd57d18c54bd
virav15 <47110597+virav15@users.noreply.github.com>
2019-07-18 12:46:21 -0400

    --- Auto-Git Commit ---

---
+++ .history/app_20190718124114.py
@@ -0,0 +1,30 @@
+import dash
+
+app = dash.Dash()
+server = app.server
+app.config.suppress_callback_exceptions = True
+
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app
+server = app.server
+from apps import world, country
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+@app.callback(Output('page-content', 'children'),
+             [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/country':
+        return country.layout
+    else:
+        return world.layout
+
+if __name__ == '__main__':
+    app.run_server()
